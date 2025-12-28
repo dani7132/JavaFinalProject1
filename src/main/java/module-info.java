284ea9a32287307137f7251b3997e87445d050e9
dani7132 <daniyaakshulakova@gmail.com>
2025-12-28 16:34:54 +0500

    Add MySQL DB connection utility

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module com.example.javafinalproject1 {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires java.sql;
 
     opens com.example.javafinalproject1 to javafx.fxml;
     exports com.example.javafinalproject1;
